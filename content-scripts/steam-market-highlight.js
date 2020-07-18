58378738fd2a5f75be227ecf270dddef5cf6e3b3
Misha <popovychmykhailo00@gmail.com>
2020-07-18 19:40:20 +0300

    Added comments

--- content-scripts/steam-market-highlight.js
+++ content-scripts/steam-market-highlight.js
@@ -3,12 +3,12 @@ const historyButton = document.querySelector('#tabMyMarketHistory');
 
 historyButton.addEventListener('click', () => highlightLots());
 
-function highlightLots() {
-    setTimeout(function() {
+function highlightLots() { // set hightlight to lots on history page
+    setTimeout(function() { // delay before historyPage loads
 
         const lots = historyPage.querySelectorAll('.market_listing_row');
 
-        for (let lot of lots) {
+        for (let lot of lots) { // if buy lot('+') -> green highlight, if sold lot('-') -> red highlight  
             if (lot.querySelector('.market_listing_left_cell').innerHTML.includes('+')) {
                 lot.style.backgroundColor = 'rgba(50, 205, 50, 0.3)';
             } else if (lot.querySelector('.market_listing_left_cell ').innerHTML.includes('-')) {
