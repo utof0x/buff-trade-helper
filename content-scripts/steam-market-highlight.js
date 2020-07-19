0310a89a3b515d8d8218ae17013f26d30f7dd829
Misha <popovychmykhailo00@gmail.com>
2020-07-20 01:50:53 +0300

    Added support for history page numbers

--- content-scripts/steam-market-highlight.js
+++ content-scripts/steam-market-highlight.js
@@ -1,20 +1,27 @@
-const historyPage = document.querySelector('#tabContentsMyMarketHistory');
-const historyButton = document.querySelector('#tabMyMarketHistory');
+{
+    const historyPage = document.querySelector('#tabContentsMyMarketHistory');
+    const historyButton = document.querySelector('#tabMyMarketHistory');
+    
+    document.addEventListener('click', () => highlightLots(event));
+    historyButton.addEventListener('click', () => highlightLots(event));
 
-historyButton.addEventListener('click', () => highlightLots());
+    function highlightLots(event) { // set hightlight to lots on history page
+        const target = event.target;
+        
+        if (target.className != 'market_paging_pagelink' && target.className != 'market_tab_well_tab_contents') return;
 
-function highlightLots() { // set hightlight to lots on history page
-    setTimeout(function() { // delay before historyPage loads
+        setTimeout(function() { // delay before historyPage loads
 
-        const lots = historyPage.querySelectorAll('.market_listing_row');
+            const lots = historyPage.querySelectorAll('.market_listing_row');
 
-        for (let lot of lots) { // if buy lot('+') -> green highlight, if sold lot('-') -> red highlight  
-            if (lot.querySelector('.market_listing_left_cell').innerHTML.includes('+')) {
-                lot.style.backgroundColor = 'rgba(50, 205, 50, 0.3)';
-            } else if (lot.querySelector('.market_listing_left_cell ').innerHTML.includes('-')) {
-                lot.style.background = 'rgba(255, 77, 77, 0.4)';
+            for (let lot of lots) { // if buy lot('+') -> green highlight, if sold lot('-') -> red highlight  
+                if (lot.querySelector('.market_listing_left_cell').innerHTML.includes('+')) {
+                    lot.style.backgroundColor = 'rgba(50, 205, 50, 0.3)';
+                } else if (lot.querySelector('.market_listing_left_cell ').innerHTML.includes('-')) {
+                    lot.style.background = 'rgba(255, 77, 77, 0.4)';
+                }
             }
-        }
 
-    }, 1000);
+        }, 1000);
+    }
 }
