ec139783c8a1b6548b31a755e22cedd07119b4e3
Misha <popovychmykhailo00@gmail.com>
2020-07-19 13:29:30 +0300

    Fixed bug when checkbox don't save and set correct value from localStorage

--- popup-page/script.js
+++ popup-page/script.js
@@ -7,18 +7,33 @@ function setInputValues() { // set value for evry input on popup page
     const inputs = document.querySelectorAll('input');
 
     for (let input of inputs) {
-        input.value = localStorage.getItem(input.className);
+        if (input.type == 'number') {
+            input.value = localStorage.getItem(input.className);
+        } else if (input.type == 'checkbox') {
+            const value = localStorage.getItem(input.className);
+            if (value === 'true') {
+                input.checked = true;
+            } else {
+                input.checked = false;
+            }    
+        }
     }
 }
 
 function saveInputValue(event) { // save new input values to chrome sync storage
     const target = event.target;
 
-    if (!target.value) return;  
     if (target.nodeName != 'INPUT') return;
+    if (target.type == 'number' && !target.value) return;
 
     const key = target.className;
-    const value = target.value;
+    let value;
+
+    if (target.type == 'number') {
+        value = target.value;
+    } else if (target.type == 'checkbox') {
+        value = target.checked;
+    }
 
     localStorage.setItem(key, value);
 }
