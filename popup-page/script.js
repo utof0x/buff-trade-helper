71b2299ce268be77ca5bf8fb80a225a843921d3a
Misha <popovychmykhailo00@gmail.com>
2020-10-05 01:17:49 +0300

    Added new function, some changes in old functions
    Added new function to change current settings tab. getName - added new case's, settings sorted in tabs order

--- popup-page/script.js
+++ popup-page/script.js
@@ -1,5 +1,6 @@
 const popupDiv = document.body.querySelector('.main');
 
+document.addEventListener('DOMContentLoaded', changeSettingsTab);
 document.addEventListener('DOMContentLoaded', () => setInputValues());
 popupDiv.addEventListener('change', () => saveInputValue(event));
 
@@ -18,6 +19,27 @@ function setInputValues() { // set value after popup page loading for every inpu
             })
         }
     }
+}
+
+function changeSettingsTab() {
+    document.querySelectorAll('.main-tabs__item').forEach( (item) => 
+        item.addEventListener('click', function (event) {
+            event.preventDefault();
+            const id = event.target.getAttribute('href').replace('#', '');
+
+            document.querySelectorAll('.main-tabs__item').forEach(
+                (child) => child.classList.remove('main-tabs__item--active')
+            );
+
+            document.querySelectorAll('.main-content__item').forEach(
+                (child) => child.classList.remove('main-content__item--active')
+            );
+
+            item.classList.add('main-tabs__item--active');
+            document.getElementById(id).classList.add('main-content__item--active');
+        })
+    );
+    document.querySelector('.main-tabs__item').click();
 }
 
 function saveInputValue(event) { // save new input values to chrome local storage
@@ -41,25 +63,45 @@ function saveInputValue(event) { // save new input values to chrome local storag
 
 function getName(name) { // convert className to normal name
     switch (name) {
-        case 'main-market-percents-content__input':
-            return 'percentsHelper';
+        case 'main-content-steam-helper__input':
+            return 'steamPercentsHelper';
 
-        case 'main-market-fee-content__input':
+        case 'main-content-steam-fee__input':
             return 'itemPriceWithoutFee';
 
-        case 'main-history-highlight-content__input':
+        case 'main-content-steam-offers__input':
+            return 'offersHelper';
+
+        case 'main-content-steam-inventory__input':
+            return 'inventoryHelper';
+
+        case 'main-content-steam-highlight__input':
             return 'historyPageHighlight';
 
-        case 'main-history-links-content__input':
+        case 'main-content-steam-links__input':
             return 'historyPageLinks';
 
-        case 'main-history-quantity-content__input':
-            return 'historyPageItemsQuantity';
+        case 'main-content-steam-quantity__input':
+            return 'itemsQuantity';
 
-        case 'main-data-currency-content__input':
+        case 'main-content-buff-helper__input':
+            return 'buffPercentsHelper';
+
+        case 'main-content-buff-theme__input':
+            return 'darkTheme';
+
+        case 'main-content-buff-inspect__input':
+            return 'inspectButton';
+
+        case 'main-content-buff-sell__input':
+            return 'buffSellHelper';
+
+        case 'main-content-global-currency__input':
             return 'cnyPrice';
 
-        }
+        case 'main-content-global-id__input':
+            return 'steamID64';
+    }
 }
 
 function getValue(name) { // get value from chrome local storage
